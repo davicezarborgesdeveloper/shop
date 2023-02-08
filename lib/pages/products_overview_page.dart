91ae56833e8206e8322c95ebc5856ba94c29959b
Davi Borges <davi.borges.developer@gmail.com>
2023-02-08 09:49:04 -0300

    Componente Item de Produto

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/component/product_item.dart';
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
@@ -12,19 +13,17 @@ class ProductsOverviewPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Minha Loja'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: GridView.builder(
-            itemCount: loadedProducts.length,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 1.5,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10),
-            itemBuilder: (ctx, i) {
-              return Text(loadedProducts[i].title);
-            }),
-      ),
+      body: GridView.builder(
+          padding: const EdgeInsets.all(10),
+          itemCount: loadedProducts.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 1.5,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10),
+          itemBuilder: (ctx, i) {
+            return ProductItem(product: loadedProducts[i]);
+          }),
     );
   }
 }
