d4c7a7665994e2ad490ee83dcc8366b10062e691
Davi Borges <davi.borges.developer@gmail.com>
2023-02-08 09:27:42 -0300

    Componente Grid de Produtos

---
+++ lib/pages/products_overview_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  final List<Product> loadedProducts = dummyProducts;
+  ProductsOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minha Loja'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+            itemCount: loadedProducts.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 1.5,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10),
+            itemBuilder: (ctx, i) {
+              return Text(loadedProducts[i].title);
+            }),
+      ),
+    );
+  }
+}
