421bf9e80e8f471ccc695daf1504eb9a0198a833
Davi Borges <davi.borges.developer@gmail.com>
2023-03-01 12:46:28 -0300

    Enviando Requisições Http

--- lib/pages/orders_page.dart
+++ lib/pages/orders_page.dart
@@ -9,17 +9,38 @@ class OrdersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus Pedidos'),
       ),
       drawer: const AppDrawer(),
-      body: ListView.builder(
-        itemCount: orders.itemsCount,
-        itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.error != null) {
+            return Center(child: Text('Ocorreu um Erro'));
+          } else {
+            return Consumer<OrderList>(
+              builder: (ctx, orders, child) => ListView.builder(
+                itemCount: orders.itemsCount,
+                itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+              ),
+            );
+          }
+        },
       ),
+      // body: _isLoading
+      // ? Center(
+      //     child: CircularProgressIndicator(),
+      //   )
+      //     : ListView.builder(
+      //         itemCount: orders.itemsCount,
+      //         itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i]),
+      //       ),
     );
   }
 }
